6c35ddb21745c91c38504cc7fb8b5f6cfa6d05ef
kaf-lamed-beyt <belac335@gmail.com>
2023-03-21 22:35:51 +0100

    feat: weird cards

--- script/data.js
+++
@@ -1,7 +0,0 @@
-const data = [
-  {
-    image:
-      "https://images.unsplash.com/photo-1679241716514-887cb8d792ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=491&q=80",
-    desc: "Heuyyyy",
-  },
-];
--- script/index.js
+++ script/index.js
@@ -1,18 +1,8 @@
 const data = [
   {
     image:
-      "https://images.unsplash.com/photo-1679241716514-887cb8d792ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=491&q=80",
-    desc: "a person standing on top of a tree trunk",
-  },
-  {
-    image:
       "https://images.unsplash.com/photo-1675539252647-12e6eb164b11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
     desc: "a person standing on top of a tree stump",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1679267382471-2ba3ece3d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
-    desc: "a view of the ceiling of a large building",
   },
   {
     image:
@@ -26,18 +16,28 @@ const data = [
   },
   {
     image:
-      "https://images.unsplash.com/photo-1675695638524-4eabf922640b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    desc: "a man standing in a tunnel with a clock tower in view",
-  },
-  {
-    image:
       "https://images.unsplash.com/photo-1675659999529-630a3febadfc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     desc: "a woman in white dress walking down a path",
   },
   {
     image:
       "https://images.unsplash.com/photo-1477282118108-2e2f8f6fbec3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    desc: "woman sitting on a bllack weight bench",
+    desc: "woman sitting on a black weight bench",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1675795921263-0781cdbedb64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=450&q=80",
+    desc: "A person holding a pair of headphones in their hand",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1444864878184-7a29cdcf4121?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    desc: "The clock at the Tower of london, i suppose",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1679267382471-2ba3ece3d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+    desc: "a view of the ceiling of a large building",
   },
 ];
 
